72564b2b5eed736c537bc8424dcb8f25dd107e62
mithwick93 <mithwick93@gmail.com>
2025-01-01 00:24:11 +0100

    add transactions table

--- src/features/transactions/components/Transactions.tsx
+++ src/features/transactions/components/Transactions.tsx
@@ -1,9 +1,117 @@
-import React from 'react';
+import { Box, Chip } from '@mui/material';
+import {
+  MaterialReactTable,
+  MRT_ColumnDef,
+  MRT_RowData,
+  useMaterialReactTable,
+} from 'material-react-table';
+import React, { useEffect, useMemo } from 'react';
+import { useData } from '../../../context/DataContext';
+import { useSettings } from '../../../context/SettingsContext';
+import {
+  formatCurrency,
+  formatTransactionType,
+  stringToColor,
+} from '../../../utils/common';
 
-const Transactions = () => (
-  <div>
-    <h2>Transactions</h2>
-  </div>
-);
+const Transactions: React.FC = () => {
+  const {
+    transactions: transactionsResponse,
+    loading,
+    refetchData,
+  } = useData();
+  const { settings } = useSettings();
+  const transactions = transactionsResponse?.content || [];
+  const searchParameters = settings?.transactions.search.parameters || {};
+  const page = searchParameters.page || 0;
+  const size = searchParameters.size || 50;
+
+  useEffect(() => {
+    refetchData(['transactions']);
+  }, [searchParameters]);
+
+  const columns = useMemo<MRT_ColumnDef<MRT_RowData>[]>(
+    () => [
+      {
+        accessorKey: 'name',
+        header: 'Name',
+        maxSize: 100,
+      },
+      {
+        accessorKey: 'description',
+        header: 'Description',
+        maxSize: 100,
+      },
+      {
+        accessorFn: (row) => formatTransactionType(row.type),
+        accessorKey: 'type',
+        header: 'Type',
+        Cell: ({ renderedCellValue }) => (
+          <Chip
+            label={renderedCellValue}
+            sx={(theme) => ({
+              backgroundColor: stringToColor(
+                renderedCellValue as string,
+                theme.palette.mode === 'dark',
+              ),
+            })}
+          />
+        ),
+      },
+      {
+        accessorFn: (row) => row.category.name,
+        accessorKey: 'category',
+        header: 'Category',
+      },
+      {
+        accessorFn: (row) => row.amount,
+        accessorKey: 'amount',
+        header: 'Amount',
+        muiTableHeadCellProps: {
+          align: 'right',
+        },
+        muiTableBodyCellProps: {
+          align: 'right',
+        },
+        Cell: ({ cell }) => (
+          <Box component="span">
+            {formatCurrency(
+              cell.row.original.amount,
+              cell.row.original.currency,
+            )}
+          </Box>
+        ),
+      },
+      {
+        accessorKey: 'date',
+        header: 'Date',
+        muiTableHeadCellProps: {
+          align: 'right',
+        },
+        muiTableBodyCellProps: {
+          align: 'right',
+        },
+      },
+    ],
+    [],
+  );
+
+  const table = useMaterialReactTable({
+    columns,
+    data: transactions,
+    enableStickyHeader: true,
+    initialState: {
+      pagination: {
+        pageIndex: page,
+        pageSize: size,
+      },
+    },
+    state: {
+      isLoading: loading,
+    },
+  });
+
+  return <MaterialReactTable table={table} />;
+};
 
 export default Transactions;
